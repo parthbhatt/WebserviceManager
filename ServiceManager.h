986d45c9c764520dc2eeae3447a566afbbf2e148
parthbhatt <parthsmac@Parthss-Mac-mini.local>
2015-10-04 14:36:50 +0530

    This is a Web Service Manager class.
    It invokes the GET and POST requests for the requested URL and returns
    the web service response along with error (if any) in the completion
    block. It internally uses NSURLSession and NSURLSessionDataTask.

---
+++ ServiceManager.h
@@ -0,0 +1,32 @@
+//
+//  ServiceManager.h
+//
+//  Created by Parth Bhatt on 21/09/15.
+//  Copyright (c) 2015 Parth Bhatt. All rights reserved.
+//
+// This is a webservice manager component which request the web and returns the error and response in completion block.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void (^ResponseCallback)(NSMutableDictionary *dictData, NSError *error);
+
+@interface ServiceManager : NSObject
+{
+    
+}
+/**
+ * This method is used to request a web service with type GET. It handles the response using block.
+ * @param serviceURL This is the service URL.
+ * @param callback It is completion block.
+ */
++(void)requestURL:(NSURL *)serviceURL responseCallback:(ResponseCallback)callback;
+
+/**
+ * This method is used to request a web service with type POST. It handles the response using block.
+ * @param serviceURL This is the service URL.
+ * @param dictParmaters It is dictionary of parameters.
+ * @param callback It is completion block.
+ */
++(void)requestURL:(NSURL *)serviceURL withDict:(NSDictionary *)dictParmaters responseCallback:(ResponseCallback)callback;
+@end
---
+++ ServiceManager.m
@@ -0,0 +1,102 @@
+//
+//  ServiceManager.m
+//  ByteClub
+//
+//  Created by Parth Bhatt on 21/09/15.
+//  Copyright (c) 2015 Parth Bhatt. All rights reserved.
+//
+// This is a webservice manager component which request the web and returns the error and response in completion block.
+//
+
+#import "ServiceManager.h"
+
+@implementation ServiceManager
+
++(void)requestURL:(NSURL *)serviceURL responseCallback:(ResponseCallback)callback
+{
+    ResponseCallback completionHandler = callback;
+    
+    NSMutableURLRequest *urlRequest = [[NSMutableURLRequest alloc] initWithURL:serviceURL];
+    [urlRequest setHTTPMethod:@"GET"];
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+    configuration.timeoutIntervalForRequest  = 60;
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)
+    {
+        if(!error)
+        {
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse*) response;
+            NSInteger responseCode = httpResponse.statusCode;
+            
+            if(responseCode == 200)
+            {
+                NSError *jsonError = nil;
+                NSMutableDictionary *jsonResponseDict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&jsonError];
+                completionHandler(jsonResponseDict,jsonError);
+            }
+            else
+            {
+                NSError *error = [[NSError alloc] initWithDomain:@"Invalid Response Status Code" code:201 userInfo:@{response.description:@"ErrorDescription"}];
+                //Error completion handler
+                completionHandler(nil,error);
+            }
+        }
+        else
+        {
+            //Error completion handler
+            completionHandler(nil,error);
+        }
+    }];
+    [dataTask resume];
+}
+
++(void)requestURL:(NSURL *)serviceURL withDict:(NSDictionary *)dictParmaters responseCallback:(ResponseCallback)callback
+{
+    NSData *jsonRequestBody = [NSJSONSerialization dataWithJSONObject:dictParmaters options:0 error:nil];
+    
+    ResponseCallback completionHandler = callback;
+    
+    NSMutableURLRequest *urlRequest = [[NSMutableURLRequest alloc] initWithURL:serviceURL];
+    
+    [urlRequest setHTTPMethod:@"POST"];
+    [urlRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [urlRequest setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    [urlRequest setValue:[NSString stringWithFormat:@"%lu",(unsigned long)jsonRequestBody.length] forHTTPHeaderField:@"Content-Length"];
+    [urlRequest setHTTPBody:jsonRequestBody];
+    
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+    config.timeoutIntervalForRequest  = 60;
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)
+                                      {
+                                          if(!error)
+                                          {
+                                              NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse*) response;
+                                              NSInteger responseCode = httpResponse.statusCode;
+                                              
+                                              if(responseCode == 200)
+                                              {
+                                                  NSError *jsonError = nil;
+                                                  NSMutableDictionary *jsonResponseDict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&jsonError];
+                                                  completionHandler(jsonResponseDict,jsonError);
+                                              }
+                                              else
+                                              {
+                                                  NSError *error = [[NSError alloc] initWithDomain:@"Invalid Response Status Code" code:201 userInfo:@{response.description:@"ErrorDescription"}];
+                                                  //Error completion handler
+                                                  completionHandler(nil,error);
+                                              }
+                                          }
+                                          else
+                                          {
+                                              //Error completion handler
+                                              completionHandler(nil,error);
+                                          }
+                                      }];
+    [dataTask resume];
+}
+
+@end
